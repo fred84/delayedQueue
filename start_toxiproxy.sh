71a8836b8a81c6d363c38a671da769b251dc9955
Sergey Galkin <sergey.galkin@revolut.com>
2019-09-26 12:52:11 +0300

    debugging using curls

--- start_toxiproxy.sh
+++ start_toxiproxy.sh
@@ -7,3 +7,6 @@ echo "[toxiproxy logs]"
 cat /tmp/toxiproxy.log
 echo "[toxiproxy process]"
 ps axu | grep toxiproxy
+echo "[curls]"
+curl -v http://localhost:8474/proxies
+curl -v http://localhost:18474/proxies
